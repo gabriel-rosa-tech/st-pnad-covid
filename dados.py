c1e76227e371e4785606b485528195603574e51b
gabrosa <gabriel.melo@bandtec.com.br>
2023-11-07 09:48:38 -0300

    analise add

---
+++ dados.py
@@ -0,0 +1,25 @@
+
+dados_pesquisa = {
+    "UF":	["Unidade da Federação"],
+    "A002":	["Idade do morador "],
+    "A003":	["Sexo"],
+    "A004":	["Cor ou raça"],
+    "B0011":	["Na semana passada teve febre?"],
+    "B0012":	["Na semana passada teve tosse?"],
+    "B0013":	["Na semana passada teve dor de garganta?"],
+    "B0014":	["Na semana passada teve dificuldade para respirar?"],
+    "B0015":	["Na semana passada teve dor de cabeça?"],
+    "B0016":	["Na semana passada teve dor no peito?"],
+    "B0017":	["Na semana passada teve náusea?"],
+    "B0018":	["Na semana passada teve nariz entupido ou escorrendo?"],
+    "B0019":	["Na semana passada teve fadiga?"],
+    "B00111":	["Na semana passada teve perda de cheiro ou sabor?"],
+    "B00112":	["Na semana passada teve dor muscular?"],
+    "B009B":	["Qual o resultado do teste?"],
+    "B011":	['Qual foi o resultado do teste? \
+        Na semana passada, devido à pandemia do Coronavírus,\
+        em que medida o(a) Sr(a) restringiu o contato com as pessoas? '],
+    "B002":	"Por causa disso, foi a algum estabelecimento de saúde?",
+    "D0051":	["Auxílios emergenciais relacionados ao coronavirus"],
+    "C011A11":	["Número da faixa do rendimento/retirada em dinheiro"]
+}
