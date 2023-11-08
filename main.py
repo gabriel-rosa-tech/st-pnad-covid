377222858d06defc1c882163f9536159d17b54e0
gabrosa <gabriel.melo@bandtec.com.br>
2023-11-08 13:51:35 -0300

    refatoracao projeto

--- main.py
+++ main.py
@@ -31,6 +31,11 @@ def apply_custom_style():
                 div[data-testid="stStatusWidget"] label{
                     color: white;
                 }
+                div[data-testid="metric-container"] {
+                    background: #264027;
+                    border-radius: 8px;
+                    
+                }
                 div[data-testid="stStatusWidget"] button{
                     color: white;
                     background: #9e829b;
@@ -265,22 +270,39 @@ FROM `notional-grove-399523.base_dados_pnad_covid.base-11-2020` -- Terceira base
 
             # Gráfico da pesquisa
             dataset_uf = pd.read_csv('./datasets_gerados/dataset_uf_pesquisa.csv')
+            
 
             # KPI dos entrevistados em cada mês
             dataset_uf = dataset_uf.rename(columns={'mes': 'Qtde entrevistados'})
+            dataset_uf = dataset_uf.sort_values('Qtde entrevistados', ascending=False)
 
+            # col1, col2 = st.columns(2)
+            # with col1:
             st.write('O levantamento feito pelo IBGE, elaborou as questões nos 26 estados e \
                         distrito federal, sendo os 3 maiores pesquisados - Minas Gerais, São Paulo e \
                         Rio de Janeiro.')
-            st.bar_chart(dataset_uf, x='UF', y='Qtde entrevistados')
+            
+            # with col2:
+            fig = px.bar(dataset_uf, x='UF', y='Qtde entrevistados', color='UF', color_discrete_sequence=px.colors.qualitative.G10)
+            st.plotly_chart(fig)
+            #st.bar_chart(dataset_uf.sort_values('Qtde entrevistados', ascending=False), x='UF', y='Qtde entrevistados')
 
             st.write('A pesquisa teve duração de 3 meses no ano de 2020, com base no gráfico se percebe \
                     qual mês houve a progressão do volume de respostas')
             
             dataset_mes = pd.read_csv('./datasets_gerados/mes_group.csv')
-            for i,r in dataset_mes.iterrows():
-                st.metric(f'{r["mes"]}', r['Qtd Entrevistados'])
-            #st.bar_chart(dataset_mes, x='mes', y='Qtd Entrevistados')
+
+            col1, col_2, col_3 = st.columns(3)
+            print(str(dataset_mes["mes"][0]))
+            print(dataset_mes['Qtd Entrevistados'][0])
+
+            col1.metric(str(dataset_mes["mes"][0]), dataset_mes['Qtd Entrevistados'][0])
+            col_2.metric(str(dataset_mes["mes"][1]), dataset_mes['Qtd Entrevistados'][1])
+            col_3.metric(str(dataset_mes["mes"][2]), dataset_mes['Qtd Entrevistados'][2])
+
+            # for i,r in dataset_mes.iterrows():
+            #     st.metric(f'{r["mes"]}', r['Qtd Entrevistados'])
+            #st.bar_chart(dataset_mes, x='mes', y='Qtd Entrevistados')  
     
             df_trabalho = pd.read_csv('./datasets_gerados/tabela-tratada-result-test-09-a-11-2020.csv') # OK
             df_trabalho = an.tratamento_df(df_trabalho)
@@ -288,30 +310,34 @@ FROM `notional-grove-399523.base_dados_pnad_covid.base-11-2020` -- Terceira base
         with st.container():
             st.subheader('1 - Caracteristicas da população')
 
-            st.write('1.1 - Distribuição de entrevistados por idades')
-            st.write('A quantidade de entrevistados se concentra na casa dos 35 aos 45')
-            scatter = alt.Chart(df_trabalho).mark_bar(width=15).encode(
-                alt.X("idade", bin=True, scale=alt.Scale(bins=[df_trabalho.idade.min(),
-                                                                df_trabalho.idade.min() + 20,
-                                                                df_trabalho.idade.min() + 40,
-                                                                df_trabalho.idade.min() + 60,
-                                                                df_trabalho.idade.min() + 80])),
-                y='count()',
-            )
-            st.altair_chart(scatter)
+            col1,  col3 = st.columns(2)
 
-            st.write('1.2 - Composição do sexo nos entrevistados')
-            st.write('Em termos de divisão a diferença de entrevistados por sexo difere por uma porcentagem baixas')
+            with col1:
+                st.write('1.1 - Distribuição de entrevistados por idades')
+                st.write('A quantidade de entrevistados se concentra na casa dos 35 aos 45')
+                
+                fig = px.histogram(df_trabalho, x="idade", nbins=20)
+                st.plotly_chart(fig)
+                #st.altair_chart(scatter)
 
-            s_sexo = df_trabalho['sexo'].value_counts(normalize=True).round(4)*100 #proporção sexo pesquisa total
-            fig = px.pie(values=s_sexo, names=s_sexo.index, color=['red', 'blue'])
-            st.plotly_chart(fig)
+                st.write('1.2 - Composição do sexo nos entrevistados')
+                st.write('Em termos de divisão a diferença de entrevistados por sexo difere por uma porcentagem baixas')
 
-            st.write('1.3 - Raças')
-            st.write('A maior parte dos entrevistados se consideram de cor parda.')
+                s_sexo = df_trabalho['sexo'].value_counts(normalize=True).round(4)*100 #proporção sexo pesquisa total
+                fig = px.pie(values=s_sexo, names=s_sexo.index, color=s_sexo.index)
+                st.plotly_chart(fig)
 
-            fig = px.bar(x=df_trabalho['cor_raca'].value_counts(ascending=False).values, y=df_trabalho['cor_raca'].unique())
-            st.plotly_chart(fig)
+            with col3:
+                st.write('1.3 - Raças')
+                st.write('A maior parte dos entrevistados se consideram de cor parda.')
+
+                fig = px.bar(x=df_trabalho['cor_raca'].value_counts(ascending=False).values, y=df_trabalho['cor_raca'].unique(), 
+                                labels={'x':'Qtde. Entrevistados', 'y':'Raças'})
+                st.plotly_chart(fig)
+
+            st.write('Por meio do levantamento dos gráficos fica visivel que existe uma balanceamento \
+                     dos dados, onde não houve muita concentração em um grupo especifico, \
+                     podendo enviesar os insights.')
 
             st.write('---')
 
@@ -329,7 +355,8 @@ FROM `notional-grove-399523.base_dados_pnad_covid.base-11-2020` -- Terceira base
             df_trabalho['resultado_teste'] = df_trabalho['resultado_teste'].fillna("Não Fez") #assumindo valores nulos como Não fez o teste
             
             fig = px.bar(x=df_trabalho["resultado_teste"].value_counts(ascending=False).values,
-                          y= df_trabalho["resultado_teste"].unique())
+                          y= df_trabalho["resultado_teste"].unique(),
+                          labels={'x':'Qtde pessoas', 'y': 'Resultado'})
             
             st.plotly_chart(fig)
 
@@ -384,7 +411,7 @@ FROM `notional-grove-399523.base_dados_pnad_covid.base-11-2020` -- Terceira base
                      dificuldade de respirar e dor no peito.')
             st.write('No gráfico abaixo é levantado o número de casos positivados que tiveram alguns destes sintomas')
 
-            fig = px.bar(df_gravidade, x='gravidade', y='resultado_teste')
+            fig = px.bar(df_gravidade, x='gravidade', y='resultado_teste', color='gravidade')
             st.plotly_chart(fig)
 
             st.write('Levando em conta o comportamento daqueles que estavam com covid mas eram assintomáticos \
@@ -392,8 +419,12 @@ FROM `notional-grove-399523.base_dados_pnad_covid.base-11-2020` -- Terceira base
                      pessoas')
             
             df_assintomaticos_positivados = df_trabalho_positivo[df_trabalho_positivo["gravidade"] == "Assintomático"]
+            
             fig = px.bar(x=df_assintomaticos_positivados['fez_isolamento'].unique(),
-                          y=df_assintomaticos_positivados['fez_isolamento'].value_counts(ascending=False).values)
+                          y=df_assintomaticos_positivados['fez_isolamento'].value_counts(ascending=False).values,
+                          labels={'x': 'Adoção do isolamento', 'y':'Qtde. pessoas'}
+                        #   color='x'
+                          )
             st.plotly_chart(fig)
 
             # st.subheader("Relação entre familias positivadas versus famílias sintomáticas ( 2 ou + sintomas)")
@@ -438,6 +469,18 @@ FROM `notional-grove-399523.base_dados_pnad_covid.base-11-2020` -- Terceira base
         # Caracteristicas economicas
         with st.container():
             st.subheader('4 - Caracteristicas economicas x casos')
+
+            st.write('Afim de entender o perfil classes enconômicas com a relação dos positivados \
+                     o gráfico abaixo mostra a porcentagem de pessoas que recebem seus rendimentos \
+                     em uma faixa estipulada, consideram=ndo somente aqueles que tiveram a \
+                     confirmação de Covid-19 ')
+
+            df_positivo_faixa = df_positivos['faixa_rendimento'].value_counts(normalize=True).round(4)*100
+            fig = px.bar(df_positivo_faixa, x=df_positivo_faixa.index,
+                         labels={'index':'Faixa de rendimento', 'faixa_rendimento':'Porcentagem'},
+                         color=df_positivo_faixa.index,
+                         y='faixa_rendimento')
+            st.plotly_chart(fig)
 
     with tab_conclusao:
         st.subheader('Ações a serem tomadas em novo surto')
